5b96b528547a9d2abebebf1850810b3e637f14d5
Anton Fedorov <datacompboy@gmail.com>
2024-11-22 22:37:55 +0100

    Add upstream version check :)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Windows;
 using System.Threading;
 using System.IO;
+using System.Net.Http;
+using System.Text.Json;
 
 
 namespace kat_pcgw_nexus
@@ -13,10 +15,13 @@ namespace kat_pcgw_nexus
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly string JsonUrl = "https://gatewayservice.katvr.com/api/v1/nexus/lists";
+
         public MainWindow()
         {
             Directory.SetCurrentDirectory("C:\\Program Files (x86)\\KAT Gateway\\");
             InitializeComponent();
+            Loaded += MainWindow_Loaded; // Attach the event handler
             NexusService.Instance.BroadcastMessageReceived += OnMessageReceived;
             NexusService.Instance.StartListening();
         }
@@ -35,6 +40,45 @@ namespace kat_pcgw_nexus
         {
             NexusService.Instance.StopListening();
             base.OnClosing(e);
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await FetchAndDisplayJsonAsync();
+        }
+
+        private async Task FetchAndDisplayJsonAsync()
+        {
+            try
+            {
+                using var client = new HttpClient();
+                string json = await client.GetStringAsync(JsonUrl);
+
+                // Optionally parse JSON if you're looking for specific fields
+                var parsedJson = JsonDocument.Parse(json);
+                var fieldValue = parsedJson.RootElement.GetProperty("data")[0].GetProperty("nexusVersion").GetString();
+
+                var isVerOkay = "";
+                if (fieldValue == "2.1.5")
+                {
+                    isVerOkay = "[All good] ";
+                }
+                else if (fieldValue.StartsWith("2.1."))
+                {
+                    isVerOkay = "[Should be OK] ";
+                }
+                else
+                {
+                    isVerOkay = "[WARNING] ";
+                }
+
+                // Update TextBox
+                JsonTextBox.Text = $"{isVerOkay}Upstream Nexus version: {fieldValue}. This application should be good for 2.1.x versions.";
+            }
+            catch (Exception ex)
+            {
+                JsonTextBox.Text = $"Error fetching upstream Nexus: {ex.Message}";
+            }
         }
 
     }
